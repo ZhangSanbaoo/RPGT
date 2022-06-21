6e62b0735e88fc4883959aaa34a5783cf36f9307
ZhangSanbaoo <melub.the.magician@gmail.com>
2022-06-21 22:00:23 +0200

    添加动作方法（有bug）

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from logging import critical
-from telnetlib import theNULL
+from os import WIFSIGNALED
+from signal import pause
+from telnetlib import WILL, theNULL
+import os
 
 
 class Monster:
     Mcount = 0
+
     def __init__(self, name, hp, att, deff, spd):
         self.name = name
         self.hp = hp
@@ -12,49 +16,10 @@ class Monster:
         self.spd = spd
         Monster.Mcount += 1
         self.id = Monster.Mcount
+        self.jdt = 0
 
-    def battle(self, bcounter):
-        print("%s与%s相遇了。" % (HeroU.name, self.name))
-        if self.spd > HeroU.spd:
-            bcounter = 1
-            print("%s对%s发动了攻击" % (self.name, HeroU.name))
-            self.bp(bcounter)
-        else:
-            bcounter = 2
-            print("%s对%s发动了攻击" % (HeroU.name, self.name))
-            self.bp(bcounter)
-
-    def bp(self, bcounter):
-        while True:
-            if bcounter % 2 == 0:
-                diff = HeroU.att - self.deff
-                bcounter += 1
-                if diff <= 0:
-                    print("%s造成了0点伤害。" % HeroU.name)
-                else:
-                    self.hp -= diff
-                    print("%s造成了%d点伤害。" % (HeroU.name, diff))
-                    if self.hp > 0:
-                        pass
-                    else:
-                        print("%s被打败了" % self.name)
-                        break
-            else:
-                diff = self.att - HeroU.deff
-                bcounter += 1
-                if diff <= 0:
-                    print("%s造成了0点伤害" % self.name)
-                else:
-                    HeroU.hp -= diff
-                    print("%s造成了%d点伤害" % (self.name, diff))
-                    if HeroU.hp > 0:
-                        pass
-                    else:
-                        print("%s被打败了" % HeroU.name)
-                        break
-
-    def battlegroupe(self):
-        print("%s进入战斗状态。" % HeroU.name)
+    def __str__(self):
+        return "Name: %s, HP: %s, Att: %s, Deff: %s, Speed: %s, ID: %s" % (self.name, self.hp, self.att, self.deff, self.spd, self.id)
 
 
 class Hero:
@@ -65,12 +30,58 @@ class Hero:
         self.deff = deff
         self.spd = spd
 
-bcounter = 0
-Monster1 = Monster("Orc", 50, 5, 2, 2)
-print(Monster1.name,Monster1.hp,Monster1.att,Monster1.deff,Monster1.spd,Monster1.id)
-Monster2 = Monster("Hilichurl", 100, 15, 5, 5)
-print(Monster2.name,Monster2.hp,Monster2.att,Monster2.deff,Monster2.spd,Monster2.id)
+    def Hattack(self, list_monster):
+        list_monster[0].hp -= self.att
 
+
+def battle(HeroU, list_monster) -> None:
+    round = True
+    while round:
+        list_monster = list_monster
+        if len(list_monster) == 0:
+            print("%s获得了胜利！" % HeroU.name)
+            round = False
+        else:
+            HeroU.jdt += HeroU.spd
+            if HeroU.jdt >= 20:
+                if act() == 1:
+                    pass
+                if act() == 2:
+                    pass
+                if act() == 3:
+                    pass
+                if act() == 4:
+                    pass
+                if act() == 5:
+                    pass
+            for i in list_monster:
+                i.jdt += i.spd
+                if i.stat == False:
+                    list_monster.remove(i)
+
+
+def act():
+    try:
+        print("你的回合。")
+        selection = int(input(
+            "请选择你的行动： 1.攻击\n                 2.技能\n                 3.防御\n                 4.物品\n                 5.逃跑\n您的选择："))
+        if selection > 5 or selection < 1:
+            raise ValueError('请输入1-5之间的数字！')
+        else:
+            return selection
+    except ValueError:
+        print("请输入1-5之间的数字！")
+        act()
+
+HeroU = Hero("鲁卡", 100, 20, 15, 5)
+Monster1 = Monster("Orc", 100, 15, 8, 2)
+Monster2 = Monster("Hilichurl", 80, 12, 6, 3)
+
+list_monster = []
+list_monster.append(Monster1)
+list_monster.append(Monster2)
+# battle(HeroU, list_monster)
+print(act())
 # Hname = input("请输入勇者姓名：")
 # Hhp = int(input("请输入勇者的生命值："))
 # Hatt = int(input("请输入勇者的攻击数值："))
@@ -81,3 +92,43 @@ print(Monster2.name,Monster2.hp,Monster2.att,Monster2.deff,Monster2.spd,Monster2
 
 # Monster1.battle(bcounter)
 # Monster2.battle(bcounter)
+
+# def battle(self, bcounter):
+#     print("%s与%s相遇了。" % (HeroU.name, self.name))
+#     if self.spd > HeroU.spd:
+#         bcounter = 1
+#         print("%s对%s发动了攻击" % (self.name, HeroU.name))
+#         self.bp(bcounter)
+#     else:
+#         bcounter = 2
+#         print("%s对%s发动了攻击" % (HeroU.name, self.name))
+#         self.bp(bcounter)
+
+#     def bp(self, bcounter):
+#         while True:
+#             if bcounter % 2 == 0:
+#                 diff = HeroU.att - self.deff
+#                 bcounter += 1
+#                 if diff <= 0:
+#                     print("%s造成了0点伤害。" % HeroU.name)
+#                 else:
+#                     self.hp -= diff
+#                     print("%s造成了%d点伤害。" % (HeroU.name, diff))
+#                     if self.hp > 0:
+#                         pass
+#                     else:
+#                         print("%s被打败了" % self.name)
+#                         break
+#             else:
+#                 diff = self.att - HeroU.deff
+#                 bcounter += 1
+#                 if diff <= 0:
+#                     print("%s造成了0点伤害" % self.name)
+#                 else:
+#                     HeroU.hp -= diff
+#                     print("%s造成了%d点伤害" % (self.name, diff))
+#                     if HeroU.hp > 0:
+#                         pass
+#                     else:
+#                         print("%s被打败了" % HeroU.name)
+#                         break
